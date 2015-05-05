ebf8d5b5e7cda5575ba00917afd1f44117a6a0a2
Christophe Quignon <christophe.quignon@recogizer.de>
2015-05-05 18:46:53 +0200

    included to predict dataset into training set, welcome to timeseries

--- random_trees/randomtree.py
+++ random_trees/randomtree.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from imp import load_source
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from random import randrange
 import numpy as np
+import pylab
 
 #import dataset
 #equivalent to:
@@ -15,59 +16,61 @@ ds = helper.dsimport()
 ds = helper.stretch(ds)
 
 #subsampling
-ds = helper.subsample_days(ds)
-
-#pick training, test and validation data
-train_set = ds
-test_set = []
-validation_set = []
-
-test_percentage = 0.15
-validation_percentage = 0.15
-
-test_items = int(test_percentage*len(train_set))
-validation_items = int(validation_percentage*len(train_set))
-
-for _ in range(test_items):
-    rand_elem = train_set.pop(randrange(len(train_set)))
-    test_set.append(rand_elem)
-
-for _ in range(validation_items):
-    rand_elem = train_set.pop(randrange(len(train_set)))
-    validation_set.append(rand_elem)
+# ds = helper.subsample_hours(ds)
+ds = helper.subsample_n_minutes(ds, 2)# do it, only takes a few minutes!
 
 
+hour = 0.000176125 #percentage value of one hour for the whole dataset
+test_percentage = 7 * 24 * hour# 0.000352315 = one hour
 
+test_items = int(test_percentage*len(ds))
 
-output_keys = ['Energie', 'Leistung']
+#pick a random testset
+# for _ in range(test_items):
+#     rand_elem = train_set.pop(randrange(len(train_set)))
+#     test_set.append(rand_elem)
 
-train_in, train_out = helper.split(train_set, output_keys)
-test_in, test_out = helper.split(test_set, output_keys)
-validation_in, validation_out = helper.split(test_set, output_keys)
+# take the last n values as testset
+test_set = ds[-test_items:]
+train_set = ds[:-test_items]
 
+#OUTPUT
+output_keys = ['Energie', 'Leistung', 'Volumenstrom']
+# output_keys = ['Ruecklauftemperatur', 'Volumenstrom']
 
-train_in, _ = helper.as_matrix(train_in)
+#looks a little complicated, mabye we just slice...
+_, train_out = helper.split(train_set, output_keys)
 train_out, _ = helper.as_matrix(train_out)
 
-
-test_in, _ = helper.as_matrix(test_in)
+_, test_out = helper.split(test_set, output_keys)
 test_out, _ = helper.as_matrix(test_out)
 
+#TRAIN SET
+#The output and the input don't have to be disjoint for prediction disjoint
+train_set, _ = helper.split(train_set, []) #splits off the dates
+train_set, _ = helper.as_matrix(train_set)
 
-validation_in, _ = helper.as_matrix(validation_in)
-validation_out, _ = helper.as_matrix(validation_out)
+#TEST SET
+#The output and the input don't have to be disjoint for prediction disjoint
+test_set, _ = helper.split(test_set, []) #splits off the dates
+test_set, _ = helper.as_matrix(test_set)
 
 
-# # Create the random forest object which will include all the parameters
-# # for the fit
-forest = RandomForestClassifier(n_estimators = 100)
-#
-# # Fit the training data to the Survived labels and create the decision trees
-forest = forest.fit(train_in, train_out)
-#
-# # Take the same decision trees and run it on the test data
-output = forest.predict(validation_in)
+#train forest
+forest = RandomForestRegressor(n_estimators = 100)
+forest = forest.fit(train_set, train_out)
+output = forest.predict(test_set)
 
-print (output -  validation_out)
-print np.mean(output -  validation_out)
-print 'yay, we predicted something'
+for i in range(output.shape[1]):
+    pylab.figure(figsize=(20,10)) #King size output
+    pylab.title('Regression for ' + helper.translate(output_keys[i]) )
+    #TODO: check if title is right
+    pylab.plot(output[:, i], color = 'blue')
+    pylab.plot(test_out[:, i], color = 'red')
+
+    pylab.tight_layout()
+    pylab.savefig('../img/prediction-' + str(int(test_percentage/hour))+ helper.translate(output_keys[i]) + '.png')
+    # pylab.show()
+
+print forest.score(test_set, test_out)
+print 'zu schoen um wahr zu sein...'
