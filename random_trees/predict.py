a7d9a41352e2a9a305eb4a9107c594f9a275db72
Christophe Quignon <christophe.quignon@recogizer.de>
2015-05-22 14:15:28 +0200

    wrapper included

--- random_trees/predict.py
+++ random_trees/predict.py
@@ -8,6 +8,7 @@ import pylab
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
+import csv
 
 #import dataset
 #equivalent to:
@@ -21,116 +22,157 @@ ds = helper.dsimport()
 df = pd.DataFrame(ds)
 df.set_index(df.Date, inplace = True)
 df.interpolate(inplace=True)
-df = df.resample('10Min')
+df = df.resample('1Min')
 df.fillna(inplace=True, method='ffill')#we at first forwardfill
 df.fillna(inplace=True, method='bfill')#then do a backwards fill
 
 
-def slice(ds, timedelta_input, timedelta_output, to_predict, freq=1):
+def slice(ds, timedelta_input, timedelta_output, to_predict, stepwidth, input_sampling, output_sampling):
     """
-    Slices a dataframe into inputs and putputframes and ruturns them along with their shape.
+    Slices a dataframe into inputs and outputframes and returns them along with their shape.
+    The stepwidth of the sampling, the size of the frames and the sampling inside of the frames can be defined.
     """
 
     inputs = []
     outputs = []
-    input_shape= []
-    output_shape = []
 
-    last_time = ds.index[-2] - (timedelta_input + timedelta_output)
+    start_input_frame = ds.index[0]
+    while start_input_frame +  timedelta_input + timedelta_output <= ds.index[-1]:
 
-
-
-    for idx in ds.index:
-        start_input_frame = idx
         end_input_frame = start_input_frame + timedelta_input
         end_output_frame = end_input_frame+timedelta_output
 
         input_frame = ds[start_input_frame:end_input_frame]
         output_frame = ds[end_input_frame:end_output_frame]
 
+        input_frame = input_frame.resample(input_sampling)
+        output_frame = output_frame.resample(output_sampling)
+
         for k in output_frame.keys():
             if k not in to_predict:
                 del output_frame[k]
 
-        if (input_shape == []):
-            input_shape = input_frame.shape
-            output_shape = output_frame.shape
+        input_shape = input_frame.shape
+        output_shape = output_frame.shape
 
-        if (input_frame.shape == input_shape) and (output_frame.shape == output_shape):
-            inputs.append(input_frame.as_matrix().flatten())
-            outputs.append(output_frame.as_matrix().flatten())
-        # else:
-        #     print 'Frame dropped'
-        #
-        # print input_frame.shape
-        # print output_frame.shape
-        #
+        inputs.append(input_frame.as_matrix().flatten())
+        outputs.append(output_frame.as_matrix().flatten())
+
+        #Move forward
+        start_input_frame  = start_input_frame + stepwidth
+
 
     return (inputs, input_shape), (outputs, output_shape)
 
 
+def wrapper(df, validation_delta, timedelta_input, timedelta_output, to_predict, input_sampling, output_sampling, stepwidth):
+    """wrapper for a complete prediction run"""
 
-#Define input and output frames
-timedelta_input = timedelta(hours = 3)
-timedelta_output =  timedelta(hours = 3)
-to_predict = ['Energie', "Leistung"]
+    print 'Learning:'
+    print 'Input frame: ', timedelta_input
+    print "Input sampling: ", input_sampling
+    print 'Output frame: ', timedelta_output
+    print "Output sampling: ", output_sampling
+    print 'Stepwidth: ', stepwidth
+    print 'Features to learn: ', helper.translate(to_predict)
 
-print 'Learning:'
-print 'Input frame: ', timedelta_input
-print 'Output frame: ', timedelta_output
-print 'Features learned: ', helper.translate(to_predict)
+    #cutting off the validation frame
+    last = df.index[-1] - validation_delta
 
-#cutting off the validation frame
-last = df.index[-1] - timedelta_input - timedelta_output
-last = df.index[-1] - timedelta(days = 14)
+    train_frame = df[:last]
+    validation_frame = df[last:]
 
-train_frame = df[:last]
-validation_frame = df[last:]
+    #sampling the "function"
+    (inputs, input_shape), (outputs, output_shape) = slice(train_frame, timedelta_input, timedelta_output, to_predict, stepwidth, input_sampling, output_sampling)
+    (val_in, _), (val_out, _) = slice(validation_frame, timedelta_input, timedelta_output, to_predict, stepwidth, input_sampling, output_sampling)
 
-#sampling the "function"
-(inputs, input_shape), (outputs, output_shape) = slice(train_frame, timedelta_input, timedelta_output, to_predict, 100)
-(val_in, _), (val_out, _) = slice(validation_frame, timedelta_input, timedelta_output, to_predict, 100)
+    inputs = np.asarray(inputs)
+    outputs = np.asarray(outputs)
+    val_in = np.asarray(val_in)
+    val_out = np.asarray(val_out)
 
-inputs = np.asarray(inputs)
-outputs = np.asarray(outputs)
-val_in = np.asarray(val_in)
-val_out = np.asarray(val_out)
+    print 'start learning', datetime.now()
+    forest = RandomForestRegressor(n_estimators = 10, n_jobs = 8)
+    forest = forest.fit(inputs, outputs)
+    print 'stopped learning at ', datetime.now()
 
-print 'start learning', datetime.now()
-forest = RandomForestRegressor(n_estimators = 10, n_jobs = 8)
-forest = forest.fit(inputs, outputs)
-print 'stopped learning at ', datetime.now()
+    predicted_output = forest.predict(val_in)
 
-predicted_output = forest.predict(val_in)
+    #reshape rows according to output_shape
+    validation_out = np.reshape(val_out, (val_out.shape[0], output_shape[0], output_shape[1]))
+    predicted_output = np.reshape(predicted_output, (predicted_output.shape[0], output_shape[0], output_shape[1]))
+    score = forest.score(val_in, val_out)
 
-#reshape rows according to output_shape
-validation_out = np.reshape(val_out, (val_out.shape[0], output_shape[0], output_shape[1]))
-predicted_output = np.reshape(predicted_output, (predicted_output.shape[0], output_shape[0], output_shape[1]))
-score = forest.score(val_in, val_out)
+    print 'validation:'
+    print validation_out.T
+    print ''
+    print 'prediction:'
+    print predicted_output.T
+    print ''
+    print 'score:'
+    print score
 
-print 'validation:'
-print validation_out.T
-print ''
-print 'prediction:'
-print predicted_output.T
-print ''
-print 'score:'
-print score
+    #split values
+    val_features = np.dsplit(validation_out, validation_out.shape[2])
+    pred_features = np.dsplit(predicted_output, predicted_output.shape[2])
 
-#split values
-val_features = np.dsplit(validation_out, validation_out.shape[2])
-pred_features = np.dsplit(predicted_output, predicted_output.shape[2])
+    val_features = np.squeeze(val_features, axis=2)
+    pred_features = np.squeeze(pred_features, axis=2)
 
-val_features = np.squeeze(val_features, axis=2)
-pred_features = np.squeeze(pred_features, axis=2)
+    for i, v in enumerate(val_features):
+        pylab.figure(figsize=(20,10))
+        pylab.title(helper.translate(to_predict[i]))
+        pylab.plot(np.ravel(val_features[i][:, 0], order='F'), marker="o")
+        pylab.plot(np.ravel(pred_features[i][:, 0], order='F'), marker="x")
+        in_h = timedelta_input.seconds /60/60
+        out_h = timedelta_output.seconds /60/60
+        pylab.savefig('../img/predict-' + helper.translate(to_predict[i])+ '-' + str(in_h) + str(out_h) + '--' + str(score)[:5] + '.png')
+        # pylab.show()
+        pylab.clf()
 
-for i, v in enumerate(val_features):
-    pylab.figure(figsize=(20,10))
-    pylab.title(helper.translate(to_predict[i]))
-    pylab.plot(np.ravel(val_features[i][:, 0], order='F'), marker="o")
-    pylab.plot(np.ravel(pred_features[i][:, 0], order='F'), marker="x")
-    in_h = timedelta_input.seconds /60/60
-    out_h = timedelta_output.seconds /60/60
-    pylab.savefig('../img/predict' + helper.translate(to_predict[i])+ '-' + str(in_h) + str(out_h) + '--' + str(score)[:4] + '.png')
-    pylab.clf()
-    # pylab.show()
+    return score
+
+
+def testrun():
+    """The place to set up a set of runs and write them to a csv file"""
+
+    ds = []
+
+    keys = ["validation_delta",
+            "timedelta_input",
+            "timedelta_output",
+            "to_predict",
+            "input_sampling",
+            "output_sampling",
+            "stepwidth",
+            "score"]
+
+    #Set parameters
+    validation_delta = timedelta(days = 14)
+    timedelta_input = timedelta(hours = 3)
+    timedelta_output =  timedelta(hours = 3)
+    to_predict = ['Energie', "Leistung"]
+    input_sampling = '60Min'
+    output_sampling = '60Min'
+    stepwidth = timedelta(minutes=60)
+
+    score = wrapper(df, validation_delta, timedelta_input, timedelta_output, to_predict, input_sampling, output_sampling, stepwidth)
+
+    d= {}
+    d["validation_delta"] = validation_delta
+    d["timedelta_input"] = timedelta_input
+    d["timedelta_output"] = timedelta_output
+    d["to_predict"] = to_predict
+    d["input_sampling"] = input_sampling
+    d["output_sampling"] = output_sampling
+    d["stepwidth"] = stepwidth
+    d["score"] = score
+
+    ds.append(d)
+
+    with open('../img/testrun.csv', 'wb') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(ds)
+
+testrun()
